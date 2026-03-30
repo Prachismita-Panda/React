387da535f2309bfb08c59910297f37134a19b07c
unknown <prachismitapanda10@gmail.com>
2026-03-30 18:48:26 +0530

    content added

---
+++ 300326/ecomapp/src/ColorChange.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function ColorChange(){
+    const [color,setColor] = useState("white");
+
+
+    return(
+        <>
+            <h1>{color}</h1>
+            <button onClick = {()=>setColor("Red")}>Red</button>
+            <button onClick = {()=>setColor("Green")}>Green</button>
+            <button onClick = {()=>setColor("Blue")}>Blue</button>
+        </>
+    )
+}
