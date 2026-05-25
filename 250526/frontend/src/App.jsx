7b78879a1e1fde73a1b94b6192c594b8ece83371
Prachismita-Panda <prachismitapanda10@gmail.com>
2026-05-25 07:31:55 +0530

    content added

---
+++ 250526/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import StudentInfo from "./StudentInfo"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <StudentInfo name={"Ram"} roll={101} age={23}/>
+    </>
+  )
+}
+
+export default App
---
+++ 250526/frontend/src/StudentInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StudentInfo = (props) => {
+
+  
+  return (
+    <div>
+      <h1>Roll number = {props.roll}</h1>
+      <h1>Name = {props.name}</h1>
+      <h1>Age = {props. age}</h1>
+    </div>
+  )
+}
+
+export default StudentInfo
